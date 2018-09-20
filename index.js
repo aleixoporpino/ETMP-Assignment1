c270904061de8fff367be0e1854150b4c201d2c4
aleixoporpino <aleixoporpino@gmail.com>
2018-09-19 21:07:56 -0400

    Added logs with IP address and endpoints. #4

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var app = require('./config/express-handler')();
 
-app.listen(3000, function(){
-    console.log("Server running on localhost:3000");
+app.listen(3000, function () {
+    console.log("Server is listening at http://127.0.0.1:3000/");
+    console.log("Endpoins:");
+    console.log("http://127.0.0.1:3000/sendGet method: GET");
+    console.log("http://127.0.0.1:3000/sendPost method: POST");
+    console.log("http://127.0.0.1:3000/sendDelete method: DELETE");
 });
