da2ea4c15d1b0be6d5cf12929b367e88d83b4383
Jose Aleixo Araujo Porpino Filho <aleixoporpino@gmail.com>
2018-10-03 13:42:36 -0400

    Added all the request and response console logs. closes #4

--- controllers/main-controller.js
+++ controllers/main-controller.js
@@ -12,18 +12,21 @@ var deleteCounter = 0;
 module.exports = function (app) {
     // Get all the products
     app.get("/products", function (req, res) {
+        console.log("Send request >>>");
         // Increment get counter and show the counter
         getCounter++;
         showRequestCount();
 
         //Get all the objects saved before.
         productsSave.find({}, function (error, products) {
+            console.log("Send response <<<");
             res.send(products)
         });
     });
 
     //Create a new product
     app.post("/products/new", function (req, res) {
+        console.log("Send request >>> "+ req);
         // Increment post counter and show the counter
         postCounter++;
         showRequestCount();
@@ -31,18 +34,21 @@ module.exports = function (app) {
         // Get the object in the request body, save and send the response
         var newProduct = req.body;
         productsSave.create( newProduct, function (error, product) {
+            console.log("Send response <<< " + product);
             res.send(201, product)
         });
     });
 
     // Delete all the products
     app.del('/products/deleteAll', function (req, res) {
+        console.log("Send request >>>");
         // Increment delete counter and show the counter
         deleteCounter++;
         showRequestCount();
 
         // delete all the records and send the response.
         productsSave.deleteMany({},function (error) {
+            console.log("Send response <<< All products were deleted");
             res.send(200, "All products were deleted.");
         });
     });
