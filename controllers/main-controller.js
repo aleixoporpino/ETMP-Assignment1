0a8f35a470dd2179c19976b0a3a58ecac59b771b
Jose Aleixo Araujo Porpino Filho <aleixoporpino@gmail.com>
2018-10-03 13:39:04 -0400

    Added all the necessary comments in the code and the header in each file.

--- controllers/main-controller.js
+++ controllers/main-controller.js
@@ -1,40 +1,54 @@
+/*
+ * main-controller.js manage all the requests and response http methods.
+ *
+ * Developed by Jose Aleixo Araujo Porpino Filho
+ * Date: 2018-09-29
+ * Version 1.0.0
+ */
 const productsSave = require('save')('products');
 var getCounter = 0;
 var postCounter = 0;
 var deleteCounter = 0;
 module.exports = function (app) {
+    // Get all the products
     app.get("/products", function (req, res) {
+        // Increment get counter and show the counter
         getCounter++;
         showRequestCount();
+
+        //Get all the objects saved before.
         productsSave.find({}, function (error, products) {
             res.send(products)
         });
     });
 
+    //Create a new product
     app.post("/products/new", function (req, res) {
+        // Increment post counter and show the counter
         postCounter++;
         showRequestCount();
 
-        console.log(req.body);
-
+        // Get the object in the request body, save and send the response
         var newProduct = req.body;
-
         productsSave.create( newProduct, function (error, product) {
             res.send(201, product)
         });
     });
 
     // Delete all the products
-    app.del('/products/deleteAll', function (req, res, next) {
+    app.del('/products/deleteAll', function (req, res) {
+        // Increment delete counter and show the counter
         deleteCounter++;
         showRequestCount();
+
+        // delete all the records and send the response.
         productsSave.deleteMany({},function (error) {
-            // Send a 200 OK response
             res.send(200, "All products were deleted.");
         });
     });
 };
 
+// just print in the log the counters of the requests
 function showRequestCount() {
     console.log("Processed Request Count--> sendGet:" + getCounter
         + ", sendPost:" + postCounter
--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+/*
+ * index.js file that will start the server.
+ *
+ * Developed by Jose Aleixo Araujo Porpino Filho
+ * Student ID:
+ * Date: 2018-09-29
+ * Version 1.0.0
+ */
 const app = require('./config/express-handler')();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000; // Configuration to work in Heroku
 app.listen(port, function () {
     console.log("Server is listening at http://127.0.0.1:" + port + "/");
     console.log("Endpoins:");
